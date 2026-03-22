c1014242d926656aaa7d864c40af6b9990320512
Pixtool User <user@example.com>
2026-03-22 17:13:32 +0530

    chore: Delete all application files, assets, and configurations, with minor build artifacts created.

--- src/data/constants.js
+++
@@ -1,5 +0,0 @@
-export const SITE_URL = 'https://dailytools.toolpix.in';
-export const SITE_NAME = 'DailyTools by UTHAKKAN';
-export const AUTHOR_NAME = 'Ajmal U K';
-export const AUTHOR_URL = 'https://uthakkan.in';
-export const DEFAULT_LOGO = 'https://dailytools.toolpix.in/logo.png';
